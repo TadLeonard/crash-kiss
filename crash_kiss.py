ecf1792bc2339e6f525ad9510ae7274b2463727e
tad <tadfleonard@gmail.com>
2014-07-14 02:14:55 -0400

    Preparing for image bisect / smash-to-center functionality.

--- crash_kiss.py
+++ crash_kiss.py
@@ -1,13 +1,71 @@
-from __future__ import print_function
+from __future__ import print_function, division
 from collections import namedtuple
 import argparse
 import mahotas
 import numpy as np
 
 
+##################
+# Mutating images
+
+def wall_smash_image(edges, shift_right=True):
+    """Mutates a numpy array of an image so that the subject
+    is smashed to the right or left edge."""
+    for left, right, negspace in edges:
+        rowlen = right - left
+        if not rowlen:
+            continue
+        row[-rowlen:] = row[left: right]
+        row[:-rowlen] = negspace
+
+
+def get_ascii_edges(edges, char=u"@"):
+    return "\n".join((u" " * left) + (char * (right - left))
+                     for left, right, _ in edges)
+
+
+##########################
+# Finding subject's edges
+
+row_data = namedtuple("row_data", "left_idx right_idx neg_space")
 DEFAULT_THRESH = 15
 DEFAULT_NEG_SAMPLE = 15
 
+
+def iter_subject_edges(
+        img, neg_sample=DEFAULT_NEG_SAMPLE, threshold=DEFAULT_THRESH):
+    """Finds the edges of the subject for each row of pixels. This assumes that
+    the subject is on a background with significantly different RGB values.
+    Yields `row_data` instances."""
+    for row in img:
+        yield _find_edge_indices(row, neg_sample, threshold)
+
+
+def iter_two_subject_edges(
+        img, neg_sample=DEFAULT_NEG_SAMPLE, threshold=DEFAULT_THRESH):
+    """Like `iter_subject_edges`, but for two subjects. Naively halves the
+    image and searches both halves for edges."""
+    width = img.shape[1]
+    halfway = width // 2
+    for row in img:
+        yield map(_find_edg_indices, (row[:halfway], row[halfway:]),
+                  neg_sample, threshold)
+
+
+def _find_edge_indices(row, neg_sample, threshold):
+    neg_space = np.mean(row[:neg_sample], axis=0)
+    pos_space = np.all(np.abs(row - neg_space) > threshold, axis=1)
+    left_edge = np.argmax(pos_space)
+    right_edge = np.argmax(pos_space[::-1])
+    if right_edge:
+        width = row.shape[0]
+        right_edge = width - right_edge
+    return row_data(left_edge, right_edge, neg_space)
+
+
+
+##############
+# Arg parsing
 
 parser = argparse.ArgumentParser(description="Crash images into things")
 parser.add_argument("target")
@@ -17,41 +75,6 @@ parser.add_argument("--smash", type=str,
                     choices=("left", "right", "center"), default=None)
 parser.add_argument("--output-type", type=str,
                     choices=("ascii", "gif", "img"), default="ascii")
-
-
-def wall_smash_image(img_edges, shift_right=True):
-    """Mutates a numpy array of an image so that the subject
-    is smashed to the right or left edge."""
-    for left, right, negspace in img_edges:
-        rowlen = right - left
-        if not rowlen:
-            continue
-        row[-rowlen:] = row[left: right]
-        row[:-rowlen] = negspace
-
-
-def get_ascii_edges(img_edges, char=u"@"):
-    return "\n".join((u" " * left) + (char * (right - left))
-                     for left, right, _ in img_edges)
-
-
-row_data = namedtuple("row_data", "left_idx right_idx neg_space")
-
-
-def iter_edge_indices(
-        img, neg_sample=DEFAULT_NEG_SAMPLE, threshold=DEFAULT_THRESH):
-    """Finds the edges of the subject for each row of pixels. This assumes that
-    the subject is on a background with significantly different RGB values.
-    Yields `row_data` instances."""
-    for row in img:
-        neg_space = np.mean(pixel_row[:neg_sample], axis=0)
-        pos_space = np.all(np.abs(pixel_row - neg_space) > threshold, axis=1)
-        left_edge = np.argmax(pos_space)
-        right_edge = np.argmax(pos_space[::-1])
-        if right_edge:
-            width = pixel_row.shape[0]
-            right_edge = width - right_edge
-        yield row_data(left_edge, right_edge, neg_space)
 
 
 def run():
@@ -67,7 +90,8 @@ def run():
         elif args.smash == "center":
             raise NotImplementedError("Can't smash center yet")
     mahotas.imsave("bleeeeup", img)
-    print(get_ascii_edges(img, char=u"@"))
+    edges = iter_subject_edges(img)
+    print(get_ascii_edges(edges, char=u"@"))
 
 
 if __name__ == "__main__":
