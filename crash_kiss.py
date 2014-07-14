d325ed38430f7b2a2f0db8cd9de43500f0eabb50
tad <tadfleonard@gmail.com>
2014-07-14 00:47:22 -0400

    Decoupled img mutation functions from the function used to find the subject's edges. Img mutators are now passed the edge indices.

--- crash_kiss.py
+++ crash_kiss.py
@@ -19,11 +19,10 @@ parser.add_argument("--output-type", type=str,
                     choices=("ascii", "gif", "img"), default="ascii")
 
 
-def wall_smash_image(img, shift_right=True):
+def wall_smash_image(img_edges, shift_right=True):
     """Mutates a numpy array of an image so that the subject
     is smashed to the right or left edge."""
-    for row in img:
-        left, right, negspace = get_edge_indices(row)
+    for left, right, negspace in img_edges:
         rowlen = right - left
         if not rowlen:
             continue
@@ -31,37 +30,35 @@ def wall_smash_image(img, shift_right=True):
         row[:-rowlen] = negspace
 
 
-def get_ascii_edges(img, char=u"@"):
-    return "\n".join(_gen_ascii_edges(img, char))
-
-
-def _gen_ascii_edges(img, char):
-    for row in img:
-        left, right, _ = get_edge_indices(row)
-        yield (u" " * left) + (char * (right - left))
+def get_ascii_edges(img_edges, char=u"@"):
+    return "\n".join((u" " * left) + (char * (right - left))
+                     for left, right, _ in img_edges)
 
 
 row_data = namedtuple("row_data", "left_idx right_idx neg_space")
 
 
-def get_edge_indices(
-        pixel_row, neg_sample=DEFAULT_NEG_SAMPLE, threshold=DEFAULT_THRESH):
-    """Finds the edges of the subject in a row of pixels. This assumes that
+def iter_edge_indices(
+        img, neg_sample=DEFAULT_NEG_SAMPLE, threshold=DEFAULT_THRESH):
+    """Finds the edges of the subject for each row of pixels. This assumes that
     the subject is on a background with significantly different RGB values.
-    Returns a `row_data` instance."""
-    neg_space = np.mean(pixel_row[:neg_sample], axis=0)
-    pos_space = np.all(np.abs(pixel_row - neg_space) > threshold, axis=1)
-    left_edge = np.argmax(pos_space)
-    right_edge = np.argmax(pos_space[::-1])
-    if right_edge:
-        width = pixel_row.shape[0]
-        right_edge = width - right_edge
-    return row_data(left_edge, right_edge, neg_space)
+    Yields `row_data` instances."""
+    for row in img:
+        neg_space = np.mean(pixel_row[:neg_sample], axis=0)
+        pos_space = np.all(np.abs(pixel_row - neg_space) > threshold, axis=1)
+        left_edge = np.argmax(pos_space)
+        right_edge = np.argmax(pos_space[::-1])
+        if right_edge:
+            width = pixel_row.shape[0]
+            right_edge = width - right_edge
+        yield row_data(left_edge, right_edge, neg_space)
 
 
 def run():
     args = parser.parse_args()
     img = mahotas.imread(args.target)
+    im2 = img[::, ::-1]
+    mahotas.imsave("floop", im2)
     if args.smash:
         if args.smash == "left":
             raise NotImplementedError("Can't smash left yet")
