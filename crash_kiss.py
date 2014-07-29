c36f0d814cff7b706cd9558bb463a06ecfa90074
Tad Leonard <tadfleonard@gmail.com>
2014-07-29 10:21:45 -0400

    Added a function to combine images on either the horizontal or vertical axis. Edge finding should perhaps happen BEFORE images are combined. This makes the argument parsing more straightforward. Added arguments so that nothing is done unless the user specifically asks for it.

--- crash_kiss.py
+++ crash_kiss.py
@@ -97,18 +97,6 @@ def iter_subject_edges(
         yield (_find_edge_indices(row, neg_sample, threshold),)
 
 
-def iter_two_subject_edges(
-        img, neg_sample=DEFAULT_NEG_SAMPLE, threshold=DEFAULT_THRESH):
-    """Like `iter_subject_edges`, but for two subjects. Naively halves the
-    image and searches both halves for edges."""
-    width = img.shape[1]
-    halfway = width // 2
-    find_edges = partial(_find_edge_indices,
-                         neg_sample=neg_sample, threshold=threshold)
-    for row in img:
-        yield map(find_edges, (row[:halfway], row[halfway:]))
-
-
 def iter_all_subject_edges(
         img, neg_sample=DEFAULT_NEG_SAMPLE, threshold=DEFAULT_THRESH):
     """Like `iter_subject_edges`, but for any number of
@@ -118,11 +106,11 @@ def iter_all_subject_edges(
         pos_space = np.all(np.abs(row - neg_space) > threshold, axis=1)
 
 
-def _find_edge_indices(row, neg_sample, threshold):
+def _find_edge_indices(row, neg_sample_size, threshold):
     """Find edges of a single subject. Naively assume only a left
     and a right edge are present and that there's nothing in between."""
-    neg_space_l = np.mean(row[:neg_sample], axis=0)
-    neg_space_r = np.mean(row[-neg_sample:], axis=0)
+    neg_space_l = np.mean(row[:neg_sample_size], axis=0)
+    neg_space_r = np.mean(row[-neg_sample_size:], axis=0)
     pos_space = np.all(np.abs(row - neg_space_l) > threshold, axis=1)
     left_edge = np.argmax(pos_space)
     if np.any(np.abs(neg_space_r - neg_space_l) > max(threshold // 2, 1)):
@@ -137,15 +125,29 @@ def _find_edge_indices(row, neg_sample, threshold):
     return row_data(left_edge, right_edge, neg_space_l)
 
 
+def _find_edge_indices_simple(row, neg_sample):
+    pos_space = np.all(row == neg_sample, axis=1)
+    left_edge = np.argmax(pos_space)
+    right_edge = np.argmax(pos_space[::-1])
+    return row_data(left_Edge, right_edge, neg_sample)
+
+
+def combine_images(imgs, axis="horizontal"):
+    axis = 1 if axis == "horizontal" else 0
+    combined = imgs[0]
+    for img in imgs[1:]:
+        combined = np.append(combined, img, axis=axis)
+    return combined
+
+
 ##############
 # Arg parsing
 
 parser = argparse.ArgumentParser(description="Crash images into things")
-parser.add_argument("target")
-parser.add_argument("--output", default=None)
+parser.add_argument("targets", nargs="+")
 parser.add_argument("--threshold", default=DEFAULT_THRESH, type=int)
 parser.add_argument("--smash", type=str,
-                    choices=("left", "right", "center"), default=None)
+                    choices=("left", "right"), default=None)
 parser.add_argument("--output-type", type=str,
                     choices=("char", "gif", "img"), default="char")
 parser.add_argument("--char-scale", type=float, default=1.0)
@@ -153,28 +155,41 @@ parser.add_argument("-l", "--smash_left", action="store_true",
                     default=False)
 parser.add_argument("-v", "--smash_up", action="store_true",
                     default=False)
+parser.add_argument("-a", "--combine-axis",
+                    choices=("vertical", "horizontal"), default="horizontal")
+parser.add_argument("-o", "--outfile", default=None)
+parser.add_argument("-c", "--charout", action="store_true")
+parser.add_argument("-e", "--reveal-edges", action="store_true")
 
 
 def run():
     args = parser.parse_args()
-    img = mahotas.imread(args.target)
+    imgs = map(mahotas.imread, args.targets)
+    if not imgs:
+        parser.error("Must pass in one or more paths to images")
+    if len(imgs) == 1:
+        img = imgs[0]
+    elif len(imgs) > 1:
+        img = combine_images(imgs, axis=args.combine_axis)
 
+    # After this point we're always working with one big, combined image
     if args.smash:
         if args.smash == "left":
             raise NotImplementedError("Can't smash left yet")
         elif args.smash == "right":
             edges = iter_subject_edges(img)
             wall_smash_image(img)
-        elif args.smash == "center":
-            edges = iter_two_subject_edges(img)
     else:
         edges = iter_subject_edges(img)  # TODO: hack for testing
-    #char_img = gen_char_edges(edges, char=u"@", scale=args.char_scale)
-    #print("".join(char_img))
+
+    # Various things to do with our manipulated image
     if args.reveal_edges:
         reveal_edges(edges, img)
-    if args.output:
-        mahotas.imsave(args.output, reveal_edges(edges, img))
+    if args.outfile:
+        mahotas.imsave(args.outfile, reveal_edges(edges, img))
+    if args.charout:
+        char_img = gen_char_edges(edges, char=u"@", scale=args.char_scale)
+        print("".join(char_img))
 
 
 
