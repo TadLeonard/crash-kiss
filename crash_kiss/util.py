1f27f2969e7a6045888d5466d775870de7a279ba
Tad Leonard <tadfleonard@gmail.com>
2014-08-22 14:32:05 -0400

    Edges are no longer stored as a sequence of namedtuples, but as numpy arrays per side of the image. The edges-to-text generator had to be changed to reflect this.

--- crash_kiss/util.py
+++ crash_kiss/util.py
@@ -1,16 +1,17 @@
 """Miscellaneous tools for debugging edge finding, face smashing, etc"""
 
+from six.moves import zip
+
 
 def textify_edges(edges, char=u"@", scale=1.0):
     """Generates a text representation of an image subject's edges"""
-    for edge_group in edges:
-        prev_right = 0
-        for left, right, _ in edge_group:
-            if not right:
-                continue
-            yield u" " * int((left - prev_right) * scale)
-            yield char * int((right - left) * scale)
-            prev_right = right
-        yield u"\n"
+    prev_right = 0
+    for left, right in zip(*edges):
+        if not right:
+            continue
+        yield u" " * int((left - prev_right) * scale)
+        yield char * int((right - left) * scale)
+        prev_right = right
+    yield u"\n"
 
 
