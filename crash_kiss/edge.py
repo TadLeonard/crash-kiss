8e40a887c62e250b62dd0d98c10254fb79dab273
Tad Leonard <tadfleonard@gmail.com>
2014-08-22 14:24:29 -0400

    Continued work on . The  module now finds the subject's edges with operations on the entire image array instead of working a row-by-row basis.

--- crash_kiss/edge.py
+++ crash_kiss/edge.py
@@ -1,89 +1,89 @@
-"""
+"""threshold
 Functions for finding the edges of a subject in an
 image-as-a-numpy-array
 """
 from __future__ import division
 from collections import namedtuple
+from itertools import tee
 import numpy as np
 
 
-DEFAULT_THRESH = 15
-DEFAULT_NEG_SAMPLE = 5
-
-_row_data = namedtuple("row_data", "left_idx right_idx neg_space_l neg_space_r")
-_subject_data = namedtuple("subject_data", "left right")
-_edge_data = namedtuple("row_data", "idx neg_space")
+def config(**kw_overrides):
+    conf = dict(_config_defaults)
+    conf.update(kw_overrides)
+    return conf
 
 
-def config(*overrides, **kw_overrides):
-    return _config(*overrides, **kw_overrides)
+_config_defaults = dict(
+    neg_sample_size=5,
+    threshold=7,
+    bg_change_tolerance=3,
+)
 
 
-_config = namedtuple("config",
-                     "neg_sample_size threshold bg_change_tolerance")
-_defaults = DEFAULT_NEG_SAMPLE, DEFAULT_THRESHOLD, BG_CHANGE_TOLERANCE
+def _sliding_window(iterable, size):
+    it = iter(iterable)
+    win = deque((next(it, None) for _ in xrange(size)), maxlen=size)
+    yield win
+    append = win.append
+    for e in it:
+        append(e)
+        yield win
 
 
-def iter_subject_edges(img, config=config()):
-    """Finds the edges of the subject for each row of pixels. This assumes that
-    the subject is on a background with significantly different RGB values.
-    Yields `_row_data` instances."""
-    _get_corrected_neg_space(img, neg_sample)
-    edge_neg_space = _get_edge_neg_space(img, config.neg_sample_size)
-    prev_neg_space = edge_neg_space
-    for row in img:
-        yield (_find_edge_indices(row, config.neg_sample, config.threshold),)
+def get_edges(img, config=config()):
+    overall, (left, right, up, down) = (
+        _get_edge_background(img, config["sample_size"]) 
+    left_edge = _get_edge_indices(img, left, config)
+    right_edge = _get_edge_indices(img[::, ::-1], left, config)
+    return left_edge, right_edge
 
 
-def _find_edge_indices(row, neg_sample_size, threshold, prev_edges=None):
-    """Find edges of a single subject. Naively assume only a left
-    and a right edge are present and that there's nothing in between."""
-    neg_space_l = _gather_neg_sample(row[:neg_sample_size],
-                                     threshold, prev_edges)
-    neg_space_r = _gather_neg_sample(row[-neg_sample_size:],
-                                     threshold, prev_edges)
-    pos_space = np.all(np.abs(row - neg_space_l) > threshold, axis=1)
-    left_edge = np.argmax(pos_space)
-    if np.any(np.abs(neg_space_r - neg_space_l) > max(threshold // 2, 1)):
-        pos_space_r = np.all(
-            np.abs(row[left_edge:] - neg_space_r) > threshold, axis=1)
-        right_edge = np.argmax(pos_space_r[::-1])
-    else:
-        right_edge = np.argmax(pos_space[::-1])
-    if right_edge:
-        width = row.shape[0]
-        right_edge = width - right_edge
-    return _row_data(left_edge, right_edge, neg_space_l, neg_space_r)
-
-
-def _get_corrected_neg_space(img, sample_size):
-    edge_space = _get_edge_neg_space(img, sample_size)
+def _get_edge_background(img, sample_size):
+    """Finds overall RGB background value and 
+    arrays of average "edge" RGB value for each side of the image
+    """
+    # (potentially) negative space on all four sides of the image
     left = np.median(img[::, :sample_size:], axis=1)
     right = np.median(img[::, -sample_size::], axis=1)
+    up = np.median(img[:sample_size:], axis=0)
+    down = np.median(img[-sample_size::], axis=0)
+
+    # find the median RGB value for each side
+    medians = left, right, up, down
+    medians = [np.median(med, axis=0) for med in medians]
+
+    # sort the four medians, take the mean of the middle two
+    medians = np.sort(medians, axis=0)
+    overall_neg_space = np.mean(medians[1:3], axis=0)
+    return overall_neg_space, (left, right, up, down)
 
 
-def _get_edge_neg_space(img, sample_size):
-    """Median value of negative space on the edge of the image"""
-    medians = [
-        np.median(img[:sample_size:], axis=0),
-        np.median(img[-sample_size::], axis=0),
-        np.median(img[::, :sample_size:], axis=1),
-        np.median(img[::, -sample_size::], axis=1)
-    ]
-    return np.median(medians[1:3], axis=0)
+def _get_edge_indices(img, background, config):
+    """Finds the 'edge' of the subject of an image based on a background
+    value or an array of background values. Returns an array of indices that
+    represent the first non-background pixel moving from left to right in 
+    the image. Since it finds edges from left to right, it's up to the
+    caller to pass in an appropriately inverted or rotated view of the image
+    to account for this.
+    """
+    threshold = config["threshold"]
+    bg_change_tolerance = config["bg_change_tolerance"]
 
+    # Here, we see if the background's RGB elements are similar.
+    # This way we can do a simple 
+    # array - int operation instead of the more expensive array - [R, G, B]
+    # or the even pricier array - <array of shape (NROWS, 1, 3)> operation
+    while isinstance(background, np.array):
+        bmax, bmin = background.max(axis=0), background.min(axis=0)
+        diff = (bmax - bmin) < bg_change_tolerance
+        if len(background.shape) >= 2:
+            diff = np.all(diff)
+        if diff:
+            background = np.median(background, axis=0)
+        else:
+            break  # we've simplified the background as much as we can
 
-def _gather_neg_sample(sample, threshold, prev_edges):
-    return np.mean(sample, axis=0)
-    if np.any(sample.max(axis=0) - sample.min(axis=0) > threshold):
-        return
-    else:
-        return np.mean(sample, axis=0)
-
-
-def _find_edge_indices_simple(row, neg_sample):
-    pos_space = np.all(row == neg_sample, axis=1)
-    left_edge = np.argmax(pos_space)
-    right_edge = np.argmax(pos_space[::-1])
-    return _row_data(left_Edge, right_edge, neg_sample)
+    foreground = np.all(np.abs(img - background) > threshold, axis=2)
+    return np.argmax(foreground, axis=1) 
 
