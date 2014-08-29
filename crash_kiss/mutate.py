cbee9fab5bf16cebb61fdd17bed0e4885548a93f
Tad Leonard <tadfleonard@gmail.com>
2014-08-28 20:51:28 -0400

    Moved image flipping, rotating functions to

--- crash_kiss/mutate.py
+++ crash_kiss/mutate.py
@@ -64,38 +64,3 @@ def combine_images(imgs, horizontal=True):
     return combined
 
 
-def orient_right_to_left(img):
-    return invert_horizontal(img)
-
-
-def orient_left_to_right(img):
-    return img
-
-
-def orient_down_to_up(img):
-    return rotate_cw(img)
-
-
-def orient_up_to_down(img):
-    return rotate_ccw(img)
-
-
-def invert_horizontal(img):
-    return img[::, ::-1]
-
-
-def invert_vertical(img):
-    return img[::-1]
-
-
-def rotate_180(img):
-    return img[::-1, ::-1]
-
-
-def rotate_ccw(img):
-    return img.swapaxes(0, 1)
-
-
-def rotate_cw(img):
-    return rotate_180(img).swapaxes(0, 1)
-
--- crash_kiss/util.py
+++ crash_kiss/util.py
@@ -15,3 +15,38 @@ def textify_edges(edges, char=u"@", scale=1.0):
     yield u"\n"
 
 
+def orient_right_to_left(img):
+    return invert_horizontal(img)
+
+
+def orient_left_to_right(img):
+    return img
+
+
+def orient_down_to_up(img):
+    return rotate_cw(img)
+
+
+def orient_up_to_down(img):
+    return rotate_ccw(img)
+
+
+def invert_horizontal(img):
+    return img[::, ::-1]
+
+
+def invert_vertical(img):
+    return img[::-1]
+
+
+def rotate_180(img):
+    return img[::-1, ::-1]
+
+
+def rotate_ccw(img):
+    return img.swapaxes(0, 1)
+
+
+def rotate_cw(img):
+    return rotate_180(img).swapaxes(0, 1)
+
