6faba7907830be11ea7d0aca2ec0472b87a0abd9
Tad Leonard <tadfleonard@gmail.com>
2014-08-28 20:50:37 -0400

    Continued work on  and  container

--- crash_kiss/edge.py
+++ crash_kiss/edge.py
@@ -1,116 +1,104 @@
-"""threshold
-Functions for finding the edges of a subject in an
-image-as-a-numpy-array
-"""
+"""Functions for finding the edges of the foreground in an
+image-as-a-numpy-array"""
+
 from __future__ import division
 from collections import namedtuple
 from itertools import tee
 import numpy as np
+from crash_kiss import util
 
 
 class Edges(object):
-    """Arrays of indices that represent the edges of the foreground
-    of an image"""
+    """Container of `Edge` instances, which contain arrays that define the
+    edges of the foreground of an image"""
 
     def __init__(self, img=None, config=config()):
         self._img = img
         self._config = config()
-        self.find_background(img)
-        self.find_edges(img)
         self.left = self.right = self.up = self.down = None
         self._edges = self.left, self.right, self.up, self.down
-
+        
     def __iter__(self):
         for edge in self._edges:
-            if edge:
+            if edge is not None:
                 yield edge
-        
+
+    @property
+    def background(self):
+        pass
+
+    @property
+    def foreground(self):
+        pass
+
+    @property
+    def bg_median(self):
+        bgs = [side.background for side in self]
+        sorted_bgs = np.sort(bgs, axis=0)
+        if len(bgs) == 3:
+            pass
+        elif len(bgs) == 4:
+            pass
+        else:
+            pass
+
 
 class Edge(object):
     """A single edge of the foreground of an image"""
 
-    def __init__(self, img, config):
-        self._img = img
+    def __init__(self, orientation, config=config()):
+        self._relative_side = orientation
+        self._orient = _orientators[orientation]
         self._view = None
+        self._background = None
+        self.img = None
         self._config = config
         self.find_background(img)
         self.find_edge(img) 
 
-    def find_background(self):
-        self.background = 
+    @property
+    def img(self):
+        return self._img
+
+    @img.setter
+    def img(self, new_img):
+        if new_img is None:
+            del self.img
+        else:
+            self._img = new_img
 
     @property
     def view(self):
         if self._view is None:
-            self.orient()
-        return self.
+            self._view = self._orient(self.img)
+        return self._view
 
-    def orient(self):
-        """Provides a consistent view of the image that allows `Edge` methods
-        to work as though they are all LEFT edges."""
-        raise NotImplementedError
+    @property
+    def edge(self):
+        if self._edge is None:
+            bg = self.background
+            self._edge = get_edge(self.view, bg, config)
+        return self._edge
 
     @property
     def background(self):
-        return 
-        
+        if self._background is None:
+            s_size = self._config["neg_sample_size"] 
+            self._background = get_background(self.view, s_size)
+        return self._background
+
     def __iter__(self): 
         return iter(self.edge)
-        
-        
-
-def config(**kw_overrides):
-    conf = dict(_config_defaults)
-    conf.update(kw_overrides)
-    return conf
 
 
-_config_defaults = dict(
-    neg_sample_size=5,
-    threshold=7,
-    bg_change_tolerance=3,
-)
+def get_background(img, config=config()):
+    """Returns an array of median RGB values for the background of the image
+    based on the values along the image's edges."""
+    sample_size = config["neg_sample_size"]
+    return np.median(img[::, :sample_size:], axis=0)
 
 
-def _sliding_window(iterable, size):
-    it = iter(iterable)
-    win = deque((next(it, None) for _ in xrange(size)), maxlen=size)
-    yield win
-    append = win.append
-    for e in it:
-        append(e)
-        yield win
-
-
-def get_edges(img, config=config()):
-    overall, (left, right, up, down) = (
-        _get_edge_background(img, config["neg_sample_size"]))
-    left_edge = _get_edge_indices(img, left, config)
-    right_edge = _get_edge_indices(img[::, ::-1], left, config)
-    return left_edge, right_edge
-
-
-def _get_edge_background(img, sample_size):
-    """Finds overall RGB background value and 
-    arrays of average "edge" RGB value for each side of the image
-    """
-    # (potentially) negative space on all four sides of the image
-    left = np.median(img[::, :sample_size:], axis=1)
-    right = np.median(img[::, -sample_size::], axis=1)
-    up = np.median(img[:sample_size:], axis=0)
-    down = np.median(img[-sample_size::], axis=0)
-
-    # find the median RGB value for each side
-    medians = left, right, up, down
-    medians = [np.median(med, axis=0) for med in medians]
-
-    # sort the four medians, take the mean of the middle two
-    medians = np.sort(medians, axis=0)
-    overall_neg_space = np.mean(medians[1:3], axis=0)
-    return overall_neg_space, (left, right, up, down)
-
-
-def _get_edge_indices(img, background, config):
+def get_edge(img, config=config()):
     """Finds the 'edge' of the subject of an image based on a background
     value or an array of background values. Returns an array of indices that
     represent the first non-background pixel moving from left to right in 
@@ -136,4 +124,39 @@ def _get_edge_indices(img, background, config):
 
     foreground = np.all(np.abs(img - background) > threshold, axis=2)
     return np.argmax(foreground, axis=1) 
+
+
+def config(**kw_overrides):
+    invalid = set(kw_overrides) - set(_config_defaults)
+    if invalid:
+        raise Exception("Invalid config keys: {0}".format(invalid))
+    conf = dict(_config_defaults)
+    conf.update(kw_overrides)
+    return conf
+
+
+_config_defaults = dict(
+    neg_sample_size=5,
+    threshold=7,
+    bg_change_tolerance=3,
+    use_relative_sides=("left", "right", "up", "down"),
+)
+
+
+_orientors = dict(
+    left=util.orient_left_to_right,
+    right=util.orient_right_to_left,
+    up=util.orient_up_to_down,
+    down=util.orient_down_to_up,
+)
+
+
+def _sliding_window(iterable, size):
+    it = iter(iterable)
+    win = deque((next(it, None) for _ in xrange(size)), maxlen=size)
+    yield win
+    append = win.append
+    for e in it:
+        append(e)
+        yield win
 
