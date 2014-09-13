Fixes issue #1, an off-by-one error in img slicing
* Column slicing had an off-by-one error that meant some 1-px wide
columns weren't checked for foreground/background edges. This is fixed.
* The slicing of contiguous rows was more complex than it needed to be.

crash_kiss/edge.py
@@ -168,7 +168,6 @@ def get_edge(img, background, config):
             fg = _find_foreground(img_slice, bg, config)
             sub_edge = np.argmax(fg, axis=1)
             nz_sub_edge = sub_edge != 0
-            
             sub_edge[nz_sub_edge] += prev_idx
             edge[start: stop] = sub_edge
     return edge
@@ -177,14 +176,13 @@ def get_edge(img, background, config):
 def _column_blocks(img, chunksize):
     n_cols = img.shape[1]
     chunksize = min(chunksize, n_cols)
-    n_chunks = ((n_cols // chunksize) // 2) + 1 
-    chunks = [chunksize * n for n in range(1, n_chunks)]
-    if not chunks or chunks[-1] < n_cols:
-        chunks.append(n_cols - 1)  # n_cols - 1 + 1 == n_cols
-    prev_idx = 0
-    for idx in chunks:
-        yield img[::, prev_idx: idx + 1], prev_idx
-        prev_idx = idx
+    n_chunks = (3 * (n_cols // chunksize) // 4) + 1 
+    stop_indices = [chunksize * n for n in range(1, n_chunks)]
+    if not stop_indices or stop_indices[-1] < n_cols:
+        stop_indices.append(n_cols)
+    start_indices = [0] + [s - 1 for s in stop_indices[:-1]]
+    for start, stop in zip(start_indices, stop_indices):
+        yield img[::, start: stop], start
 
 
 def _row_slices(img, edge):
@@ -197,13 +195,10 @@ def _row_slices(img, edge):
 
 
 def _get_contiguous_slice(img, z_edge, offset):
-    z_edge = z_edge[offset:]
-    start = np.argmax(z_edge)
-    if not start and not z_edge[start]:
+    start = np.argmax(z_edge[offset:]) + offset
+    if start == offset and not z_edge[start]:
         raise StopIteration
-    stop = np.argmin(z_edge[start:])
-    start += offset
-    stop += start
+    stop = np.argmin(z_edge[start:]) + start
     if stop == start:
         if z_edge[-1]:
             stop = img.shape[0]