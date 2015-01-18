d64682400c418cb298e9847f18c058e7fcdc1be8
Tad Leonard <tadfleonard@gmail.com>
2015-01-17 22:16:19 -0600

    Splitting off outer edge vs. all edge finding behavior into separate modules. Edge config functions and constants get their own module to facilitate this.

---
+++ crash_kiss/config.py
@@ -0,0 +1,28 @@
+"""Configuration of edge finding behavior"""
+
+
+def config(**kw_overrides):
+    invalid = list(set(kw_overrides) - set(_config_defaults))
+    if invalid:
+        raise Exception("Invalid config keys: {0}".format(invalid))
+    conf = dict(_config_defaults)
+    conf.update(kw_overrides)
+    return conf
+
+
+WHITE = 255  # the default white background value
+BLACK = 0
+AUTO = "auto"  # key used to auto-gather the background
+
+
+_config_defaults = dict(
+    bg_sample_size=5,
+    threshold=10,
+    bg_change_tolerance=7,
+    relative_sides=("left", "right"),
+    chunksize=300,
+    bg_value=WHITE,
+    rgb_select=None,
+)
+
+
