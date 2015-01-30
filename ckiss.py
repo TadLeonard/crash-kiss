d23b12ac2e9a09293ebff44e2cc79d423b0bf7db
Tad Leonard <tadfleonard@gmail.com>
2015-01-30 14:37:42 -0600

    added max depth arg to ckiss.py

--- ckiss.py
+++ ckiss.py
@@ -14,10 +14,10 @@ parser = argparse.ArgumentParser(
 	formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("target")
 parser.add_argument("-b", "--bg-value", type=lambda x: map(int, x.split(",")),
-                      help="A number to represent the color of the background "
-                           "should the user want to manually set it. Use "
-                           "'auto' to automatically gather per-row "
-                           "background values.",
+                    help="A number to represent the color of the background "
+                         "should the user want to manually set it. Use "
+                         "'auto' to automatically gather per-row "
+                         "background values.",
                       default=_conf["bg_value"])
 parser.add_argument("-o", "--outfile", default=None)
 parser.add_argument("-e", "--reveal-foreground", action="store_true")
@@ -27,6 +27,11 @@ parser.add_argument("-t", "--threshold",
                          "to determine an edge",
                     default=_conf["threshold"], type=int)
 parser.add_argument("-s", "--smash", action="store_true")
+parser.add_argument("-d", "--max-depth",
+                    default=_conf["max_smash_depth"], type=int,
+                    help="Max number of pixels that the left and right "
+                         "subjects will smoosh into each other. Neither face "
+                         "will collapse by more than max_depth//2")
 
 
 def main():
@@ -43,7 +48,7 @@ def main():
     if args.reveal_background:
         mutate.reveal_background(subject)
     if args.smash:
-        mutate.center_smash(img, subject)
+        mutate.center_smash(img, subject, args.max_depth)
     opts = {"quality": 100}  # no JPEG compression
     if args.outfile:
         imread.imwrite(args.outfile, img, opts=opts)
