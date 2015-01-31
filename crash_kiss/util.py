e4449e041c8f46d7da343c94af80aec2d0ffab6e
Tad Leonard <tadfleonard@gmail.com>
2015-01-31 03:09:05 -0600

    moved RGB view creation to util

--- crash_kiss/util.py
+++ crash_kiss/util.py
@@ -50,3 +50,39 @@ def rotate_ccw(img):
 def rotate_cw(img):
     return rotate_180(img).swapaxes(0, 1)
 
+
+_rgb_select = {
+    (0, 1): lambda view: view[:, :, :2],
+    (1, 2): lambda view: view[:, :, 1:3],
+    (0, 2): lambda view: view[:, :, ::2],
+    (0, 3): lambda view: view[:, :, ::3],
+    (2, 3): lambda view: view[:, :, 2:4],
+    (1, 3): lambda view: view[:, :, 1::2],
+    (2, 3): lambda view: view[:, :, 2:4],
+    (0, 1, 2): lambda view: view[:, :, :3],
+    (1, 2, 3): lambda view: view[:, :, 1:4],
+}
+
+
+def get_rgb_view(img, rgb_indices):
+    select = _get_rgb_select(img, rgb_indices)
+    # We CANNOT use advanced indexing here!
+    # Copies of large images are just too expensive.
+    if select == tuple(range(view.shape[2])):
+        return img  # we've selected ALL of RGB
+    elif len(select) == 1:
+        self._rgb_view = view[:, :, select[0]]  # just a 2-D view
+    else:
+        try:
+            return _rgb_select[select](img)  # a fancy sliced view
+        except KeyError:
+            from warnings import warn
+            warn("RGB select {0} results in a copy!".format(select))
+            return view[:, :, select]  # a nasty copy is created
+
+
+def _get_rgb_select(img, rgb_indices):
+    rgb_indices = rgb_indices or range(self.img.shape[2])
+    return tuple(sorted(set(rgb_indices)))
+
+
