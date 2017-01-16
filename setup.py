4915a1811836eb05fd3bcab427950370d7941c1e
Tad Leonard <tadfleonard@gmail.com>
2017-01-16 01:32:31 -0800

    preparing build script for OpenMP C extension

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, Extension
 try:
     import numpy
     from Cython.Build import cythonize
@@ -7,13 +7,31 @@ except ImportError:
     sys.exit("Cython and numpy required; pip install Cython numpy")
 
 
+extra_compile_args = ["-fopenmp", "-O3", "-ffast-math"]
+
+
+omp_ext = Extension("crash_kiss.omp_smoosh",
+                     sources=["crash_kiss/omp_smoosh.c",],
+                     extra_compile_args=extra_compile_args,
+                     include_dirs=["nphusl/"],
+                     extra_link_args=["-fopenmp"])
+
+
+cython_ext = Extension("crash_kiss.smoosh",
+                       sources=["crash_kiss/smoosh.pyx"],
+                       extra_compile_args=extra_compile_args,
+                       extra_link_args=["-fopenmp"])
+
+
+extensions = omp_ext, cython_ext
+
 
 setup(
     name="crash_kiss",
     scripts=["kiss.py"], 
     packages=["crash_kiss"],
-    ext_modules=cythonize("crash_kiss/smoosh.pyx"),
-    install_requires=["six", "numpy", "imread"],
+    ext_modules=cythonize(extensions),
+    install_requires=["six", "numpy", "imageio"],
     include_dirs=[numpy.get_include()],
 )
 
