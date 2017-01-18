9e86aa8da15ecd35ca8309b437dd425827245298
Tad Leonard <tadfleonard@gmail.com>
2017-01-17 19:55:01 -0800

    stub for C/OpenMP smoosh routine

---
+++ crash_kiss/omp_smoosh.c
@@ -0,0 +1,14 @@
+// C implementation of the "smoosh" operation made quicker with OpenMP
+// The "smoosh" is a process where the left and right halves of an image
+// are pushed together; foreground area is preserved and background area
+// is lost.
+
+
+#include <omp.h>
+
+
+
+void omp_smoosh() {
+    // TODO
+}
+
