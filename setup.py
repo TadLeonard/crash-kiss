805c4956ff417038cb7ce0d5a3ded9a3adb6280c
Tad Leonard <tadfleonard@gmail.com>
2017-01-31 11:21:10 -0800

    fixed wrong include dir in C setuptools ext

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ extra_compile_args = ["-fopenmp", "-O3", "-ffast-math"]
 
 
 omp_ext = Extension("crash_kiss.omp_smoosh",
-                     sources=["crash_kiss/omp_smoosh.c",],
-                     extra_compile_args=extra_compile_args,
-                     include_dirs=["nphusl/"],
-                     extra_link_args=["-fopenmp"])
+                    sources=["crash_kiss/omp_smoosh.pyx",],
+                    extra_compile_args=extra_compile_args,
+                    include_dirs=["crash_kiss/"],
+                    extra_link_args=["-fopenmp"])
 
 
 cython_ext = Extension("crash_kiss.smoosh",
@@ -28,7 +28,7 @@ extensions = omp_ext, cython_ext
 
 setup(
     name="crash_kiss",
-    scripts=["kiss.py"], 
+    scripts=["kiss.py"],
     packages=["crash_kiss"],
     ext_modules=cythonize(extensions),
     install_requires=["six", "numpy", "imageio"],
