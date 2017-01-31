e63c0f60d83e5c4bcb3a477189cc148cd23484d7
Tad Leonard <tadfleonard@gmail.com>
2017-01-31 11:20:19 -0800

    header to be included by Cython wrapper

---
+++ crash_kiss/_omp_smoosh.h
@@ -0,0 +1,10 @@
+// Header for OpenMP-optimized "smoosh" operation.
+// This is used by the Cython wrapper (_omp_smoosh_2d.pyx)
+// to provide a copy-less extension to Python/NumPy.
+
+#include <stdint.h>
+
+
+void _omp_smoosh_2d(uint8_t *img, uint8_t *foreground,
+                    int rows, int cols, int max_depth);
+
