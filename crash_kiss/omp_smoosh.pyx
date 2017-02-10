463a2eab79e3703881e8db30f6b0a232c8f6851b
Tad Leonard <tadfleonard@gmail.com>
2017-02-09 19:33:50 -0800

    Cython wrapper for OpenMP smoosh finished

--- crash_kiss/omp_smoosh.pyx
+++ crash_kiss/omp_smoosh.pyx
@@ -13,23 +13,26 @@ cdef size_t data_size = sizeof(img_t)
 
 cdef extern from "_omp_smoosh.h":
     # our _omp_smoosh_2d function from _omp_smoosh.c
-    _omp_smoosh_2d(img_t *img, img_t *foreground,
-                   int rows, int cols, int max_depth)
+    void _omp_smoosh_2d(img_t *img, img_t *foreground,
+                        int rows, int cols, int max_depth,
+                        int background_value)
 
 
-def smoosh(img, foreground, max_depth):
+def smoosh(img, foreground, max_depth, background_value=0xFFFFFF):
     cdef int rows = img.shape[0]
-    cdef int cols = img.shape[0]
+    cdef int cols = img.shape[1]
     cdef int pixels = img.size / 3
     assert pixels == foreground.size
-    rgb_flat = img.reshape((pixels, 3))
+    rgb_flat = img.reshape((img.size,))
     foreground_flat = foreground.reshape((pixels,))
-    _smoosh_2d(rgb_flat, foreground_flat, rows, cols, max_depth)
+    _smoosh_2d(rgb_flat, foreground_flat, rows, cols, max_depth,
+               background_value)
 
 
-cdef void _smoosh_2d(img_t[:, ::1] img, img_t[:, ::1] foreground,
-                     int rows, int cols, int max_depth):
-    _omp_smoosh_2d(img, foreground, rows, cols, max_depth)
-
+cdef _smoosh_2d(img_t[::1] img, img_t[::1] foreground,
+                     int rows, int cols, int max_depth,
+                     int background_value):
+    _omp_smoosh_2d(&img[0], &foreground[0], rows, cols, max_depth,
+                   background_value)
 
 
