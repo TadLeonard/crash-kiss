0975bb569146c0d7f325a5eb184793c9b6ed93d4
Tad Leonard <tadfleonard@gmail.com>
2017-02-09 19:26:16 -0800

    First working version of new OpenMP-optimized vers
    The new C impl works by maintaining left/right inner/outer cursors
    and working from the center of the image outward. It should be
    more cache-friendly, and it might be faster despite the fact that
    none of this will be vectorized. Certain edge cases (like rows of the image
    where there is NO foreground) will be slower, but really diabolical cases
    (like tons of mixed foreground/background e.g. in between human hairs)
    will be faster. Overall, it's a less complicated, more straightforward
    implementation. And, since it's in C, there are more ways to squeeze
    better performance out of it.

--- crash_kiss/_omp_smoosh.c
+++ crash_kiss/_omp_smoosh.c
@@ -5,70 +5,166 @@
 
 #include <math.h>
 #include <stdint.h>
-#include <stlib.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <omp.h>
 
 
+/*
 void _omp_smoosh_2d(uint8_t *img, uint8_t *foreground,
-                    int rows, int cols, int max_depth);
+                    int rows, int cols, int max_depth,
+                    int background_value);
+*/
 
 
 void _omp_smoosh_2d(uint8_t *img, uint8_t *foreground,
-                    int rows, int cols, int max_depth) {
+                    int rows, int cols, int max_depth,
+                    int background_value) {
     // indices for our 2D background mask array
     // we'll multiply by 3 to stride across our RGB image array
-    const int middle = cols / 2;
     const int stride = cols;  // row stride for foreground mask array
-    const int absolute_max = min(cols / 2, max_depth);
+    const int middle = stride / 2;
+    //const int absolute_max = max_depth;
+    const int absolute_max = max_depth < cols/2 ? max_depth : cols/2;
+    const int bg_rgb[] = {background_value & 0xFF,
+                          (background_value & 0xFF00) >> 8,
+                          (background_value & 0xFF0000) >> 16};
 
     int i;
-#pragma omp parallel for
+//#pragma omp parallel for
     for (i = 0; i < rows; i++) {
         // left and right "inner" cursors starting from middle
-        int l_in = stride*i + middle;
-        int r_in = stride*i + middle + 1;
-        // total number of background pixels collapsed
-        int crushed = 0;
+        int l_in = middle;
+        int r_in = middle + 1;
+        int fg_offs = i * stride;
+        int img_offs = fg_offs * 3;
+
+        // Now we find where the foreground on each side meet
+        int j;
+        for (j = l_in; j > l_in - absolute_max; j--) {
+            if (foreground[fg_offs+j]) // found the first foreground pixel on the left
+                break;
+        }
+        int l_out = j;  // left outer cursor is set
+        for (j = r_in; j < r_in + absolute_max; j++) {
+            if (foreground[fg_offs+j]) // found the first foreground pixel on the right
+                break;
+        }
+        int r_out = j;  // right outer cursor is set
+        // L and R inner cursors are at the meeting point between the subjects
+        l_in = l_out + (r_out - l_out)/2;
+        r_in = l_in + 1;
 
         // First, we find the start indices of the background
-        // on the left and right of the center line
-        int j;
+        // on the left and right of the meeting point
         for (j = l_in; j >= 0; j--) {
-            if (!foreground[j]) {
+            if (!foreground[fg_offs+j])
                 break;  // we found the background on the left
+        }
         l_in = j;
         for (j = r_in; j < cols; j++) {
-            if (!foreground[j]) {
+            if (!foreground[fg_offs+j])
                 break;  // we found the background on the right
+        }
         r_in = j;
-        if (r_in == cols && l_in == 0) {
-            // we have no foreground area in this row
-            continue;
-        }
-        // left and right "outer" cursors start just beyond inner cursors
-        int l_out = l_in - 1;
-        int r_out = r_in + 1;
 
-        // Collapse background pixels, not to exceed max_depth
-        while (crushed < absolute_max && (l_in ) {
-            if (foreground[l_out]) {
-                // Left outer cursor has selected a foreground pixel
-                l_out--;
-                foreground[l_in] = 1;
-            } else {
-                // Left inner cursor has selected a background pixel
-                crushed++;
+        // Collapse background pixels, not to exceed absolute_max
+        int k;  // RGB color channel index
+        int z;  // temp cursor index
+
+        // Crush the left half of the image
+        int l_in_last = l_in;
+        int l_out_last = l_out;
+        while (l_in && (l_in-l_out < absolute_max)) {
+            // Step 1: move outer pixel inward
+            for (k = 0; k < 3; k++) {
+                img[img_offs+l_in*3+k] = img[img_offs+l_out*3+k];
             }
-            if (foreground[ri]) {
-                // Right inner cursor has selected a foreground pixel
-                r_in++;
-            } else {
-                // Right inner cursor has selected a background pixel
-                crushed++;
+
+            // Step 2: clear/set mask on outer/inner cursors
+            foreground[fg_offs+l_in] = 1;  // inner cursor is now foreground
+            foreground[fg_offs+l_out] = 0;  // outer cursor is now background
+
+            // Step 3: Decrement until inner cursor has selected background
+            for (z = l_in; z >= 0; z--) {
+                if (!foreground[fg_offs+z])
+                    break;
             }
-            l_out--;
-            r_out++;
+            l_in_last = l_in;
+            l_in = z;
+
+            // Step 4: Decrement until outer cursor has selected foreground
+            for (z = l_out; z >= 0; z--) {
+                if (foreground[fg_offs+z])
+                    break;
+            }
+            l_out_last = l_out;
+            l_out = z;
         }
+
+        // Move remaining pixels on left
+        int l_dist = l_in_last - l_out_last;
+        for (j = l_out_last; j >= 0; j--) {
+            for (k = 0; k < 3; k++) {
+                img[img_offs + (j+l_dist)*3 + k] = 
+                    img[img_offs + j*3 + k];
+            }
+        }
+
+        // Clear vacated space on left edge
+        for (j = l_dist; j >= 0; j--) {
+            for (k = 0; k < 3; k++) {
+                img[img_offs + j*3 + k] = bg_rgb[k];
+            }
+        }
+
+        // Crush the right half of the image
+        int r_in_last = r_in;
+        int r_out_last = r_out;
+        while ((r_in < cols-1) && (r_out-r_in < absolute_max)) {
+            // Step 1: move outer pixel inward
+            for (k = 0; k < 3; k++) {
+                img[img_offs+r_in*3+k] = img[img_offs+r_out*3+k];
+            }
+
+            // Step 2: clear/set mask on outer/inner cursors
+            foreground[fg_offs+r_in] = 1;  // inner cursor is now foreground
+            foreground[fg_offs+r_out] = 0;  // outer cursor is now background
+
+            // Step 3: Increment until inner cursor has selected background
+            for (z = r_in; z <= cols-1; z++) {
+                if (!foreground[fg_offs+z])
+                    break;
+            }
+            r_in_last = r_in;
+            r_in = z;
+
+            // Step 4: Increment until outer cursor has selected foreground
+            for (z = r_out; z <= cols-1; z++) {
+                if (foreground[fg_offs+z])
+                    break;
+            }
+            r_out_last = r_out;
+            r_out = z;
+        }
+
+        // Move remaining pixels on right
+        int r_dist = r_out_last - r_in_last;
+        for (j = r_out_last; j <= cols-1; j++) {
+            for (k = 0; k < 3; k++) {
+                img[img_offs + (j-r_dist)*3 + k] = 
+                    img[img_offs + j*3 + k];
+            }
+        }
+
+        // Clear vacated space on left edge
+        for (j = cols-1-r_dist; j <= cols-1; j++) {
+            for (k = 0; k < 3; k++) {
+                img[img_offs + j*3 + k] = bg_rgb[k];
+            }
+        }
+
+
     }
 }
 
