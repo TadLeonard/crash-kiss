2a799bdbf50e229bf7f5d58ebdefd6819320b0f9
Tad Leonard <tadfleonard@gmail.com>
2017-02-09 19:29:54 -0800

    simplified foreground-finding code
    Got rid of the "rgb select" feature which let the user
    consider one/more of the three RGB channels. It wasn't useful.
    Simplified the background value processing code so that
    the background value can be a 24-bit integer (i.e. 0xFFFFFF for white).
    This means the user can pass in ANY RGB color into the crash kiss
    process and it'll find the fg/bg based on this bg color.

--- crash_kiss/foreground.py
+++ crash_kiss/foreground.py
@@ -1,11 +1,11 @@
 """Functions for determining which parts of an image are in the foreground
 and which parts are in the background.
 
-The goal is to use a `threshold` to compare pixels to a `bg_value`  
+The goal is to use a `threshold` to compare pixels to a `bg_value`
 (a background value, usually something to represent the color white).
 If we have a pixel P of three dimensions (R, G, B), P is considered to be
 a part of the 'foreground' if ANY of R, G, or B is different enough than
-`bg_value`. In other words, 
+`bg_value`. In other words,
 `is_foreground = any(color - threshold > threshold for color in pixel)"""
 
 from __future__ import division
@@ -22,18 +22,7 @@ def find_foreground(img, params):
     to a simple int or float, we'll try to avoid calling `np.abs`
     by checking to see if the background value is near 0 or 255."""
     view, bounds = get_foreground_area(img, params.max_depth)
-    view = util.get_rgb_view(view, params.rgb_select)
-    if len(view.shape) == 2:
-        return compare_background(view, params.bg_value, params.threshold)
-    rgb_views = [view[:, :, idx] for idx in range(view.shape[-1])]
-    
-    # the foreground is a 2D array where 1==foreground 0==background
-    fg = compare_background(rgb_views[0], params.bg_value, params.threshold)
-    for rgb_view in rgb_views[1:]:
-        bg = fg == 0
-        new_data = compare_background(
-            rgb_view[bg], params.bg_value, params.threshold)
-        fg[bg] = new_data
+    fg = compare_background(view, params.bg_value, params.threshold)
     return fg, bounds
 
 
@@ -43,23 +32,38 @@ def trim_foreground(img, foreground, params):
     start = bounds.start - difference
     stop = bounds.stop - difference
     return foreground[:, start: stop], bounds
- 
+
 
 def compare_background(img, background, threshold):
     """Compare a 2-D or 3-D image array to a background value given a
     certain threshold"""
     is_num = isinstance(background, int)
-    if background - BLACK <= 5:
+    if is_num and background > 0xFF:
+        # check to see if our background value represents more
+        # than a single channel, convert to RGB tuple if so
+        r = background & 0xFF
+        g = (background & 0xFF00) >> 8
+        b = (background & 0xFF0000) >> 16
+        background = r, g, b
+        is_num = False
+    if not is_num and (background[0] == background[1] == background[2]):
+        # optimize for BG values like 0xFFFFFF or 0x808080 or 0x00
+        # This makes use of NumPy's more efficient broadcasting
+        is_num = True
+        background = background[0]
+    if is_num and (background - BLACK <= 5):
+        # optimize for BG values that are essentially black
         diff = img - background > threshold
-    elif WHITE - background <= 5:
+    elif is_num and (WHITE - background <= 5):
+        # optimize for BG values that are essentially white
         diff = background - img > threshold
     else:
         diff = np.abs(img - background) > threshold
     if len(diff.shape) == 3:
         diff = np.any(diff, axis=2)  # we're using a 3D array
     return diff.astype(np.uint8)
-   
-    
+
+
 def get_foreground_area(img, max_depth):
     """Make a slice of the middle of the image based on `max_depth`.
     Returns a view of the middle section of `img` and a `namedtuple`
